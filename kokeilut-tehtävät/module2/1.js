4b647361dd39757943271b29da1d491682f5235f
ttinnar <111982172+ttinnar@users.noreply.github.com>
2022-11-09 17:20:40 +0200

    tehtäviä

---
+++ kokeilut-tehtävät/module2/1.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const nro1 = prompt('Anna numero')
+const nro2 = prompt('Anna numero')
+const nro3 = prompt('Anna numero')
+const nro4 = prompt('Anna numero')
+const nro5 = prompt('Anna numero')
+
+const numbers = []
+numbers.sort((nro1,nro5) => nro5-nro1);
+  console.log(numbers);
