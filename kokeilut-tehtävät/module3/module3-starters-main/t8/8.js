ae1e5a46b13f9c7fe83a29042c0248a1874612c2
ttinnar <111982172+ttinnar@users.noreply.github.com>
2022-11-28 13:50:58 +0200

    tehtäviä

--- kokeilut-tehtävät/module3/module3-starters-main/t8/8.js
+++ kokeilut-tehtävät/module3/module3-starters-main/t8/8.js
@@ -1,6 +1,27 @@
-'use strict'
-
 // 1. valitse elementti / elementit
 // 3. kun nappia klikataan, hae arvot ja tee haluttu laskutoimitus
+// ---split merkkijono talukukoksi //
 
-// ---split merkkijono talukukoksi
+'user strict'
+const btn = document.getElementById('start')
+const result = document.getElementById('result')
+btn.addEventListener('click', a=>{
+    const mode = document.getElementById('operation').value
+    const nums = [+document.getElementById('num1').value,
+            +document.getElementById('num2').value]
+
+    switch(mode){
+        case 'add':
+            val = nums[0] + nums[1]
+            break
+        case 'sub':
+            val = nums[0] - nums[1]
+            break
+        case 'multi':
+            val = nums[0] * nums[1]
+            break
+        case 'div':
+            val = nums[0] / nums[1]
+    }
+    result.innerHTML = val
+})
